9b8c8ecc54f102df29e7d703b3c9c0f38658001b
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-03-01 01:46:53 +0100

    Add files via upload

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,1 +1,19 @@
+//require mongoose to connect
+const mongoose = require('mongoose')
+require("dotenv").config() 
+
+//require dotenv to get the MONGO_URI
+const MONGO_URI = process.env.MONGO_URI
+
+ //connecting to Atlas database
+
+ const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log("An error has occured while trying to connect")
+    }
+}
+module.exports= connectDB ;
 

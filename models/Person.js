ed7b2baa5f8d767ec28df0481e4d152ea49f5f95
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-03-01 01:48:09 +0100

    Add files via upload

--- models/Person.js
+++ models/Person.js
@@ -1,1 +1,19 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema; 
+// schema is used to define the structure of document data
 
+//defining the schema
+const PersonSchema=new Schema(
+    {
+        name: {
+            type:String,
+            required:true
+        },
+        age:Number,
+        favoriteFoods:[String]
+    }
+)
+
+//defining the model
+const Person=mongoose.model('Person',PersonSchema)
+module.exports=Person;
